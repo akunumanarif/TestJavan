2e8df8a7a2d30cf20b89f96b1e0dad4a7b5f9d49
M Nu'man Arif H <numanarif87@gmail.com>
2022-12-15 01:25:52 +0700

    Setup MongoDB Conn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://numanarif:akunumanarif@cluster0.ecx0a.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB connection Succesful"))
+  .catch((err) => console.log(err));
+
 const PORT = 5000;
 
 app.listen(PORT, () => {

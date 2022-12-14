f413d3d4b60faacc1503d4ad94fc2a307225073b
M Nu'man Arif H <numanarif87@gmail.com>
2022-12-15 05:23:43 +0700

    Jawaban soal nomor 1

---
+++ soalNoSatu.js
@@ -0,0 +1,114 @@
+db.testjavan.insertMany([
+  { nama: "Bani", aset: [] },
+  {
+    nama: "Budi",
+    aset: [
+      {
+        asetName: "Samsung Universe 9",
+        asetPrice: 1249,
+        _id: "639a47783aba50fb3cea9b2d",
+      },
+      {
+        asetName: "Samsung Galaxy Book",
+        asetPrice: 1499,
+        _id: "639a47783aba50fb3cea9b2e",
+      },
+    ],
+  },
+  {
+    nama: "Hari",
+    aset: [
+      {
+        asetName: "iPhone 9",
+        asetPrice: 549,
+        _id: "639a47ac3aba50fb3cea9b31",
+      },
+    ],
+  },
+  {
+    nama: "Siti",
+    aset: [
+      {
+        asetName: "iPhone X",
+        asetPrice: 899,
+        _id: "639a47d83aba50fb3cea9b34",
+      },
+    ],
+  },
+  {
+    nama: "Nida",
+    aset: [
+      {
+        asetName: "Huawei P30",
+        asetPrice: 499,
+        _id: "639a48003aba50fb3cea9b37",
+      },
+    ],
+  },
+  {
+    nama: "Bila",
+    aset: [
+      {
+        asetName: "Samsung Universe 9",
+        asetPrice: 1249,
+        _id: "639a482d3aba50fb3cea9b3a",
+      },
+    ],
+  },
+  {
+    nama: "Lesti",
+    aset: [
+      {
+        asetName: "Huawei P30",
+        asetPrice: 499,
+        _id: "639a48843aba50fb3cea9b3d",
+      },
+      {
+        asetName: "iPhone X",
+        asetPrice: 899,
+        _id: "639a48843aba50fb3cea9b3e",
+      },
+    ],
+  },
+  {
+    nama: "Andi",
+    aset: [
+      {
+        asetName: "Samsung Universe 9",
+        asetPrice: 1249,
+        _id: "639a48a73aba50fb3cea9b41",
+      },
+    ],
+  },
+  {
+    nama: "Diki",
+    aset: [
+      {
+        asetName: "Samsung Galaxy Book",
+        asetPrice: 1499,
+        _id: "639a48fc3aba50fb3cea9b53",
+      },
+    ],
+  },
+  {
+    nama: "Sigit",
+    aset: [
+      {
+        asetName: "Huawei P30",
+        asetPrice: 499,
+        _id: "639a49203aba50fb3cea9b56",
+      },
+    ],
+  },
+  {
+    nama: "Doni",
+    aset: [
+      {
+        asetName: "iPhone X",
+        asetPrice: 899,
+        _id: "639a493f3aba50fb3cea9b59",
+      },
+    ],
+  },
+  { nama: "Toni", aset: [] },
+]);
